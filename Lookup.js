47a7e6ff3f9654ccad5006f824f74e2404d529b2
Danielle Malcolm <danielle.malcolm@mymona.uwi.edu>
2018-11-13 17:28:50 +0000

    Neeted things up and added some new files

---
+++ Lookup.js
@@ -0,0 +1,9 @@
+window.onload=main;
+
+function main(){
+
+   document.getElementById("submission").addEventListener("click",function(){
+       window.alert(" A statement of the exact meaning of a word, especially in a dictionary.");
+   });
+
+}
